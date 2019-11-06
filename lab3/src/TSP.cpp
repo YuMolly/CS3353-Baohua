251dddfad84be5831ef8b33d320216041327291c
YuMolly <42575271+YuMolly@users.noreply.github.com>
2019-11-06 14:45:24 -0600

    v2

--- lab3/src/TSP.cpp
+++ lab3/src/TSP.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <string>
+#include <math.h>
+#include <utility>
 using namespace std;
 
 
@@ -13,44 +15,64 @@ TSP::TSP()
 
 void TSP::BF(vector<vector<float>> a ,int x)
 {
-	vector<string> path;
+	vector<pair<string,float>>path;
+	vector<string>per_path;
 	string my_path;
 	int total_path = 1;
 	bool status;
 	for (int i = 1; i < x; i++)
 		total_path *= i;
-	//cout << "The total_path is: " << total_path << endl;
 	// create the first path
 	for (int j = 1; j <= x; j++) {
 		my_path += to_string(j);
 	}
 	//push the first path
-	path.push_back(my_path);
+	float dist = 0.0;
+	dist = distance(a, my_path);
+	path.push_back(make_pair(my_path,dist));
 	int counter = 0;
-	cout << "my_path is: " << my_path << endl;
+	
 	for (int k = 0; k < total_path; k++) {
-		for (int i = 1; i < x-1; i++) {
+		for (int i = 1; i < x - 1; i++) {
 			swap(my_path[i], my_path[i + 1]);
-			for (int j = 1; j < x -1; j++) {
-				swap(my_path[i+1], my_path[j + 1]);
-				//swap(my_path[j + 1], my_path[j]);
-				
-				
-
-				//cout << "The new path is " << my_path << endl;
+			for (int j = 1; j < x - 1; j++) {
+				swap(my_path[i + 1], my_path[j + 1]);
 				status = check(path, my_path);
 				if (status == false) {
 					counter++;
-					cout << "My new path is: " << my_path << endl;
-					path.push_back(my_path);
+					//calculate the distance 
+					dist = distance(a, my_path);
+					path.push_back(make_pair(my_path, dist));
 				}
 			}
 		}
 	}
-	cout << "total path is: " << counter +1<< endl;
 	for (int i = 0; i < path.size(); i++) {
-		//cout << "The path is: " << path[i] << endl;
+		cout << "The path is: " << path[i].first << " ";
+		cout << "and the size is: " << path[i].second << endl;
 	}
+	cout << "total path is: " << counter + 1 << endl;
+}
+
+vector<string> TSP::permutation(string path, int start, int end)
+{  
+	int i;
+	vector<string>p;
+	if (start == end) {
+		cout << "The path is: " << path << endl;
+		p.push_back(path);
+		//return path;
+	}
+	else
+	{
+		for (i = start; i< path.length(); i++)
+		{
+			swap(path[start], path[i]);
+			permutation(path, i+1, end);
+			swap(path[start], path[i]);
+		}
+	}
+	return p;
 }
 
 void TSP::DP(vector<vector<float>> a, int x)
@@ -60,16 +82,40 @@ void TSP::DP(vector<vector<float>> a, int x)
 }
 
 
-float TSP::distance(vector<int> path)
+
+float TSP::distance(vector<vector<float>> graph,string path)
 {
+	int path_s = 0;
+	vector<int> path_t;
+	// convert char to int
+	for (int i = 0; i < path.length(); i++) {
+		path_s = path[i] - '0';
+		path_t.push_back(path_s);
+	}
+
+	float dist = 0.0;
+	int x, y, z;
+	int x1, y1, z1;
+	int location;
+	for (int j = 0; j < path_t.size()-1; j++) {
+		location = path_t[j];
+		x = graph[location][0];
+		y = graph[location][1];
+		z = graph[location][2];
+		x1 = graph[location + 1][0];
+		y1 = graph[location + 1][1];
+		z1= graph[location + 1][2];
+		dist += pow(2, (x1 - x)) + pow(2, (y1- y)) + pow(2, (z1 - z));
+		
+	}
 	
-	return 0.0;
+	return dist;
 }
 
-bool TSP::check(vector<string>path,string y)
+bool TSP::check(vector<pair<string,float>>path,string y)
 { 
 	for (int i = 0; i < path.size(); i++) {
-		if (y == path[i])
+		if (y == path[i].first)
 			return true;
 	}
 	return false;
--- lab3/src/TSP.h
+++ lab3/src/TSP.h
@@ -2,13 +2,16 @@
 #define TSP_hpp
 #include<vector>
 #include<string>
+#include<utility>
+
 class TSP {
 public:
 	TSP();
 	void BF(std::vector<std::vector<float>>,int);
+	std::vector<std::string> permutation(std::string, int,int);
 	void DP(std::vector<std::vector<float>>,int);
-	float distance(std::vector<int>);
-	bool check(std::vector<std::string>,std::string);
+	float distance(std::vector<std::vector<float>>,std::string);
+	bool check(std::vector<std::pair<std::string,float>>,std::string);
 private:
 	
 	std::vector<std::vector<float>> graph;
